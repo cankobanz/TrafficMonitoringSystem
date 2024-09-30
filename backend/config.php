eff63e8ccc9efe2db067cf1590c03bdca1bd736a
Can Koban <cankoban.ck@gmail.com>
2024-09-30 12:39:27 +0200

    configs are finalized.

--- backend/config.php
+++ backend/config.php
@@ -1,7 +1,7 @@
 <?php
 // MongoDB Configuration
 define('MONGODB_URI', 'mongodb://localhost:27017');
-define('MONGODB_DBNAME', 'traffic_project_test');
+define('MONGODB_DBNAME', 'anwb_traffic_data');
 
 // API Configuration
 define('API_URL', 'https://api.anwb.nl/v2/incidents?apikey=QYUEE3fEcFD7SGMJ6E7QBCMzdQGqRkAi&polylines=true&polylineBounds=true&totals=true');
--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
-const api_url_time = '2024-09-30T06:53:00.251Z';  // Set this variable to null or empty if you don't want to filter by time. Ex: 2024-09-29T21:05:57.812Z
-// const api_url_time = ''
+// const api_url_time = '2024-09-30T06:53:00.251Z';  // Set this variable to null or empty if you don't want to filter by time. Ex: 2024-09-29T21:05:57.812Z
+const api_url_time = ''
 // Construct the API URL based on whether the time parameter is provided
 export const API_URL = api_url_time 
     ? `http://localhost:8000/backend/api.php?time=${api_url_time}`  // Use the time query parameter
